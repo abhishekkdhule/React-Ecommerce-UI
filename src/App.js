21630d0f38c279bf667fa24cfce106865512e74c
abhishek <abhishek.dhule.79@gmail.com>
2021-06-07 21:46:35 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Products from './Products' ;
 import Payment from './Payment'
+import Nav from './Nav'
 function App() {
   return (
     <>
-    <h1> Product </h1>
+    <Nav/>
+    <h1 className="text-center"> Product </h1>
     <div>
       <Products/>
     </div>
