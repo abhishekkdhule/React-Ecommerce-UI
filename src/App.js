0c689f1a65cf0b1174e193e5d9335e139cc25f10
abhishek <abhishek.dhule.79@gmail.com>
2021-06-16 09:42:10 +0530

    react router installed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import Products from './Products' ;
 import Payment from './Payment/Payment'
 import Nav from './Navbar/Nav'
 import Signup from './Auth/Signup';
+import Signin from './Auth/Signin';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <>
     <Nav/>
     <div>
-      {/* <Signup/> */}
-      <Products/>
       {/* <Cart/> */}
     </div>
+    <Router>
+    <Switch>
+          <Route exact path="/">
+            <Products/> 
+          </Route>
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+          <Route path="/signin">
+            <Signin/>
+          </Route>
+    </Switch>
+    </Router>
     </>
   );
 }
