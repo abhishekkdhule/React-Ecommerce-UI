f4aa5aaa0fa6fad9342aae0348112e6e5da6c929
abhishek <abhishek.dhule.79@gmail.com>
2021-06-17 12:58:29 +0530

    got token while logging in

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   return (
     <>
     <Nav/>
-
     <Router>
     <Switch>
           <Route exact path="/">
--- src/Auth/Signin.jsx
+++ src/Auth/Signin.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
+import axios from 'axios'
+import React,{useEffect, useState} from 'react'
+import {Redirect,useHistory} from "react-router-dom"
 import './auth.css'
 
 function Signin() {
-    return (
-        <div class="container">
-        <div class="row mt-4">
-            <div class="col-md-4 mx-auto">
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const history=useHistory()
+    const [errors,setErrors]=useState(null)
+
+    const Login=()=>{
+        axios.post('http://127.0.0.1:8000/auth/login/',{"email":email,"password":password})   
+        .then(response=>{
+            console.log(response.data)
+            history.push("/")
+            
+        })
+        .catch(error=>{
+            console.log(error)
+            setErrors("Invalid Credentials!")
+        })
+    }
+
+    // useEffect(()=>{
+    //     axios.post('http://127.0.0.1:8000/auth/login/',{"email":email,"password":password})   
+    //     .then(response=>{
+    //         console.log(response)
+    //     })
+    //     .catch(error=>{
+    //         console.log(error)
+    //     })
+    // })
+
+    console.log(email,password)
+    return (<>
+        
+        <div className="container">
+        <div className="row mt-4">
+            <div className="col-md-4 mx-auto">
                 <div className="border rounded m-1 p-3 shadow">
-                    <h3 class="text-center">Sign in</h3>
+                    {
+                        errors && <div class="alert alert-danger p-2 mt-2 text-center" role="alert">
+                        {errors}
+                        </div>
+                    }
+                    <h3 className="text-center">Sign in</h3>
                     
-                    <label for="email" class="form-label m-0 mt-2">Email</label>
-                    <input type="text" id="email" class="form-control"  name="email"/>
+                    <label for="email" className="form-label m-0 mt-2">Email</label>
+                    <input type="text" id="email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)} name="email"/>
 
-                    <label for="password" class="form-label m-0 mt-2">Password</label>
-                    <input type="text" id="password" class="form-control"  name="password"/>
+                    <label for="password" className="form-label m-0 mt-2">Password</label>
+                    <input type="text" id="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} name="password"/>
 
-                    <button class="btn btn-primary w-100 mt-3">Sign in</button>
-                    
+                    <button className="btn btn-primary w-100 mt-3" onClick={Login}>Sign in</button>
                     <hr/>
-                    <div class="d-flex justify-content-evenly">
-                        <i class="fab fa-google icon"></i> <i class="fab fa-facebook icon"></i>
+                    <div className="d-flex justify-content-evenly">
+                        <i className="fab fa-google icon"></i> <i className="fab fa-facebook icon"></i>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+  
+    </>
     )
 }
 
